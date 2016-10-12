2c1100cfe63afbfdd3a24a192a0f1286c831fc03
gandhark <abhijeetka@cybage.com>
2016-10-12 10:22:00 +0530

    initial commit

---
+++ backup_jmeter/selenium/test.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+DEBUG=''
+
+if [ -n "$1" ] && [ $1 == 'debug' ]; then
+  DEBUG='-debug'
+fi
+
+echo Building test container image
+docker build -t selenium/test:local ./Test
+
+echo 'Starting Selenium Hub Container...'
+HUB=$(docker run -d selenium/hub:2.47.1)
+HUB_NAME=$(docker inspect -f '{{ .Name  }}' $HUB | sed s:/::)
+echo 'Waiting for Hub to come online...'
+docker logs -f $HUB &
+sleep 2
+
+echo 'Starting Selenium Chrome node...'
+NODE_CHROME=$(docker run -d --link $HUB_NAME:hub  selenium/node-chrome$DEBUG:2.47.1)
+echo 'Starting Selenium Firefox node...'
+NODE_FIREFOX=$(docker run -d --link $HUB_NAME:hub selenium/node-firefox$DEBUG:2.47.1)
+docker logs -f $NODE_CHROME &
+docker logs -f $NODE_FIREFOX &
+echo 'Waiting for nodes to register and come online...'
+sleep 10s
+
+
+echo 'calling function test_node_firefox'
+ test_node_firefox
+ {
+  BROWSER=firefox
+  echo Running $BROWSER test...
+  TEST_CMD="node smoke-$BROWSER.js"
+  docker run -it --link $HUB_NAME:hub -e TEST_CMD="$TEST_CMD" selenium/test:local
+  STATUS=$?
+  TEST_CONTAINER=$(docker ps -aq | head -1)
+
+  if [ ! $STATUS = 0 ]; then
+    echo Failed
+    exit 1
+  fi
+
+  if [ ! "$CIRCLECI" = "true" ]; then
+    echo Removing the test container
+    docker rm $TEST_CONTAINER
+ fi
+
+}
+
+
+
+echo '****************************************************************************'
+echo 'calling function test_node_chrome'
+test_node_chrome
+ {
+  BROWSER=chrome
+  echo Running $BROWSER test...
+  TEST_CMD="node smoke-$BROWSER.js"
+  docker run -it --link $HUB_NAME:hub -e TEST_CMD="$TEST_CMD" selenium/test:local
+  STATUS=$?
+  TEST_CONTAINER=$(docker ps -aq | head -1)
+
+  if [ ! $STATUS = 0 ]; then
+    echo Failed
+    echo '*****************'
+    exit 1
+  fi
+
+  if [ ! "$CIRCLECI" = "true" ]; then
+    echo Removing the test container
+    docker rm $TEST_CONTAINER
+ fi
+
+        }
+
+echo '****************************************************************************'
+
+
+test_node_chrome $DEBUG
+test_node_firefox $DEBUG
+
+#if [ ! "$CIRCLECI" ==  "true" ]; then
+  echo Tearing down Selenium Chrome Node container
+  docker stop $NODE_CHROME
+  docker rm $NODE_CHROME
+
+
+  echo Tearing down Selenium Firefox Node container
+  docker stop $NODE_FIREFOX
+  docker rm $NODE_FIREFOX
+
+  echo 'At last removing hub'
+  docker stop $HUB
+  docker rm $HUB
+#fi
+
+echo Done
